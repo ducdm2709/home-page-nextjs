277137016f5dd8fe2ef7867dc7d6811046fb7a3c
ducdm2709 <ducdm2709@gmail.com>
2020-04-09 12:33:14 +0700

    fix container

--- pages/Components/Footer/index.js
+++ pages/Components/Footer/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./footer.module.scss";
 const Footer = () => {
   return (
-    <div className={`row ${styles.footer}`}>
-      <div className="container-fluid d-flex justify-content-between">
+    <div className={` ${styles.footer}`}>
+      <div className="container p-0 d-flex justify-content-between">
         <img
           className={`logo__img mt-3 ${styles.logoFooter}`}
           src="/assets/logo-white.png"
